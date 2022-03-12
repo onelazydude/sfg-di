4bb1b2a7aee14f7d84bbc8f73cb7bd28dbcb2312
nikunjchapagain <chapagain.nikunj@gmail.com>
2022-03-11 20:54:14 -0600

    Added primary to understand qualifiers

--- src/main/java/com/chapagaintechnologies/sfgdi/controller/MyController.java
+++ src/main/java/com/chapagaintechnologies/sfgdi/controller/MyController.java
@@ -1,11 +1,16 @@
 package com.chapagaintechnologies.sfgdi.controller;
 
+import com.chapagaintechnologies.sfgdi.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    @Autowired
+    private GreetingService greetingService;
+
     public String sayHello() {
-        System.out.println("Hello World");
-        return "Hi Folks";
+        return greetingService.sayGreeting();
     }
 }
