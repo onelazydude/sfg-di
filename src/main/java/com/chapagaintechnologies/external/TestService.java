33f311f3fa7a06d9bf830cae61b7ef204cfc0932
nikunjchapagain <chapagain.nikunj@gmail.com>
2022-03-13 16:38:12 -0500

    Added component scan changes

---
+++ src/main/java/com/chapagaintechnologies/external/TestService.java
@@ -0,0 +1,10 @@
+package com.chapagaintechnologies.external;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public String test(){
+        return "This  is just a test";
+    }
+}
--- src/main/java/com/chapagaintechnologies/sfgdi/SfgDiApplication.java
+++ src/main/java/com/chapagaintechnologies/sfgdi/SfgDiApplication.java
@@ -4,7 +4,9 @@ import com.chapagaintechnologies.sfgdi.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.chapagaintechnologies.sfgdi","com.chapagaintechnologies.external"})
 @SpringBootApplication
 public class SfgDiApplication {
 
